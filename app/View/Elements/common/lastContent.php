26656842537bf05c2f525c809dcbd73b5e1fec39
takasefuhito <dr.fuhito@gmail.com>
2015-12-06 11:30:00 +0900

    mypage upadate

--- app/View/Elements/common/lastContent.php
+++ app/View/Elements/common/lastContent.php
@@ -1,4 +1,5 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo $this->webroot?>js/jquery-2.1.4.min.js"><\/script>')</script>
 <script src="<?php echo $this->webroot?>js/jquery.easing.min.js"></script>
+<script src="<?php echo $this->webroot?>js/jQueryAutoHeight.js"></script>
 <script src="<?php echo $this->webroot?>js/jquery.common.js"></script>
--- app/View/Mypage/index.php
+++ app/View/Mypage/index.php
@@ -17,9 +17,37 @@
         </aside><!-- /#mypage-menu -->
 
         <div id="main-area" class="layout-main layout-r">
-            <section id="weathcer-news">
-                <img src="http://placehold.jp/48/ffebfa/ffffff/720x400.png?text=%20%E7%BE%8E%E5%AE%B9%E5%A4%A9%E6%B0%97%E4%BA%88%E5%A0%B1%E3%81%AE%E3%82%B3%E3%83%B3%E3%83%86%E3%83%B3%E3%83%84" class="fitimg-w" alt="">
-            </section><!-- /#weathcer-news -->
+            <section id="mp-advisory">
+                <header>
+                    <h2 class="advisory-head"><img src="<?php echo WEBROOT; ?>images/mypage/advisory-head.png" class="fitimg-w" alt="あなたの美容天気予報"></h2>
+                    <div class="advisory-status">
+                        <ul>
+                            <li><dl><dt>登録地域</dt><dd>鹿児島県 種子島・屋久島地方 種子島</dd></dl></li>
+                            <li><dl><dt>肌質</dt><dd>健康な肌</dd></dl></li>
+                        </ul>
+                    </div><!-- /.advisory-status -->
+                </header>
+                
+                <div class="section-body">
+                    <section class="advisory-dryair level1">
+                        <h2><img src="<?php echo WEBROOT; ?>images/mypage/dryair1.png" alt="" class="fitimg-w"></h2>
+                        <div class="section-body">
+                            <img src="<?php echo WEBROOT; ?>images/mypage/advisory-boxtop.png" alt="" class="fitimg-w">
+                            <p class="js-ah-xooo">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                            <img src="<?php echo WEBROOT; ?>images/mypage/advisory-boxbtm.png" alt="" class="fitimg-w">
+                        </div><!-- /.section-body -->
+                    </section><!-- /.advisory-dryair -->
+                    
+                    <section class="advisory-sunburn level2">
+                        <h2><img src="<?php echo WEBROOT; ?>images/mypage/sunburn2.png" alt="" class="fitimg-w"></h2>
+                        <div class="section-body">
+                            <img src="<?php echo WEBROOT; ?>images/mypage/advisory-boxtop.png" alt="" class="fitimg-w">
+                            <p class="js-ah-xooo">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                            <img src="<?php echo WEBROOT; ?>images/mypage/advisory-boxbtm.png" alt="" class="fitimg-w">
+                        </div><!-- /.section-body -->
+                    </section><!-- /.advisory-sunburn -->
+                </div>
+            </section><!-- /#mp-advisory -->
 
             <section id="mp-news">
                 <h2 class="head-bar ico-arrow">Beauty Postからのお知らせ</h2>
