38d080d8464a9248fbcb08995e2cb3dfc63e8c76
takasefuhito <dr.fuhito@gmail.com>
2015-12-17 01:36:52 +0900

    + guide up

--- app/View/Pages/guide.php
+++ app/View/Pages/guide.php
@@ -1,1 +1,106 @@
-guide
+<div id="page-area">
+	<div class="layout">
+		<ol id="breadcrumb" class="breadcrumb rsp-xxoo">
+			<li><a href="<?php echo WEBROOT; ?>">Beauty Post</a></li>
+			<li>使い方ガイド</li>
+		</ol><!-- /#breadcrumb -->
+		
+		<div id="main-area" class="layout-main layout-l">
+			<h2 class="pagetitle"><!-- 
+				 --><img src="<?php echo WEBROOT; ?>images/guide/pagetitle-full.jpg" width="1984" height="256" class="fitimg-w rsp-xooo" alt="Beauty Postご利用ガイド guide"><!-- 
+				 --><img src="<?php echo WEBROOT; ?>images/guide/pagetitle-half.jpg" width="768" height="184" class="fitimg-w rsp-oxxx" alt="Beauty Postご利用ガイド guide"><!-- 
+			 --></h2>
+			<p></p>
+			
+			<section id="guide-search">
+				<h2 class="head-bar ico-arrow"><span class="inner">美容機器探し</span></h2>
+				<section class="flowbox">
+					<h2 class="flowtab">1.探す</h2>
+					<section class="flow01">
+						<h2><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow01-head.png" alt="" class="fitimg-w rsp-xxoo"><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow01-head-half.png" alt="" class="fitimg-w rsp-ooxx"><!-- 
+						 --></h2>
+						<div class="flowtxt">
+							<p>機器の種類や目的、部位などのさまざまな条件から、あなたに合った美容機器が見つけられます。</p>
+						</div>
+						<div class="flowimg">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow01-img.jpg" alt="" class="fitimg-w rsp-xxoo">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow01-img-half.jpg" alt="" class="fitimg-w rsp-ooxx">
+						</div>
+					</section>
+				</section>
+				
+				<div class="flownext">
+					<img src="<?php echo WEBROOT; ?>images/guide/next01.png" class="fitimg-w" alt="">
+				</div>
+				
+				<section class="flowbox">
+					<h2 class="flowtab">2.検討する</h2>
+					<section class="flow02 js-ah-xxoo">
+						<h2><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow02-head.png" alt="" class="fitimg-w rsp-xxoo"><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow02-head-half.png" alt="" class="fitimg-w rsp-ooxx"><!-- 
+						 --></h2>
+						<div class="flowtxt">
+							<p>商品発売日や価格はもちろん、保証期間や機能などのスペック比較ができます。</p>
+						</div>
+						<div class="flowimg">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow02-img.jpg" alt="" class="fitimg-w rsp-xxoo">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow02-img-half.jpg" alt="" class="fitimg-w rsp-ooxx">
+						</div>
+					</section>
+					<section class="flow03 js-ah-xxoo">
+						<h2><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow03-head.png" alt="" class="fitimg-w rsp-xxoo"><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow03-head-half.png" alt="" class="fitimg-w rsp-ooxx"><!-- 
+						 --></h2>
+						<div class="flowtxt">
+							<p>商品へのみんなの評価やBeauty Postを通じて実際に体験したモニターの方たちの生声がチェックできます。</p>
+						</div>
+						<div class="flowimg">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow03-img.jpg" alt="" class="fitimg-w rsp-xxoo">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow03-img-half.jpg" alt="" class="fitimg-w rsp-ooxx">
+						</div>
+					</section>
+					<footer>
+						<a href="<?php echo WEBROOT; ?>signup/" class="button btn-pk btn-sizeS">会員登録はこちら</a>
+					</footer>
+				</section>
+				
+				<div class="flownext">
+					<img src="<?php echo WEBROOT; ?>images/guide/next02.png" class="fitimg-w" alt="">
+				</div>
+				
+				<section class="flowbox">
+					<h2 class="flowtab">3.保存する</h2>
+					<section class="flow04">
+						<h2><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow04-head.png" alt="" class="fitimg-w rsp-xxoo"><!-- 
+						 --><img src="<?php echo WEBROOT; ?>images/guide/flow04-head-half.png" alt="" class="fitimg-w rsp-ooxx"><!-- 
+						 --></h2>
+						<div class="flowtxt">
+							<p>「Want！」ボタンで気に入った美容機器をクリップしておくことができます。</p>
+						</div>
+						<div class="flowimg">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow04-img.jpg" alt="" class="fitimg-w rsp-xxoo">
+							<img src="<?php echo WEBROOT; ?>images/guide/flow04-img-half.jpg" alt="" class="fitimg-w rsp-ooxx">
+						</div>
+					</section>
+					<footer>
+						<a href="<?php echo WEBROOT; ?>signup/" class="button btn-pk btn-sizeS">会員登録はこちら</a>
+					</footer>
+				</section>
+			</section><!-- /#guide-search -->
+			
+			<div id="guide-last" class="container">
+				<img src="<?php echo WEBROOT; ?>images/guide/fin.png" class="fitimg-w rsp-xxoo" alt="">
+				<img src="<?php echo WEBROOT; ?>images/guide/fin-half	.png" class="fitimg-w rsp-ooxx" alt="">
+			</div><!-- /#guide-last -->
+		</div><!-- /#main-area -->
+		
+		<aside id="sub-area" class="layout-sub layout-r rsp-xxxo">
+			<?php echo $this->element('common/cmnSubContent'); ?>
+		</aside><!-- /#sub-area -->
+	</div>
+</div><!-- /#page-area -->
